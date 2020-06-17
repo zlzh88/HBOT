084a0fc1aeae023ef0d0b740bfa6fce65f39a471
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2020-06-17 21:23:22 +0900

    Update Testing Date Skill
    공무원 시험 일정 스킬 제작

--- skill-server/Groom Server/HBOT_flask/application.py
+++ skill-server/Groom Server/HBOT_flask/application.py
@@ -26,7 +26,7 @@ def Message():
         "listCard": {
           "header": {
             "title": "코로나 관련 뉴스입니다!",
-            "imageUrl": "https://proxy.goorm.io//service/5edf4a94cfcc8f3ea83d67ff_dp9Hbo59aZ4kbCsRvUr.run.goorm.io/9080//file/load/corona.jpg?path=d29ya3NwYWNlJTJGSEJPVF9mbGFzayUyRmNvcm9uYS5qcGc=&docker_id=dp9Hbo59aZ4kbCsRvUr&secure_session_id=cLVVoywMLcGY7mTcJfIqJcZMOYySkqMn"
+            "imageUrl": "https://proxy.goorm.io//service/5edf4a94cfcc8f3ea83d67ff_dp9Hbo59aZ4kbCsRvUr.run.goorm.io/9080//file/load/covid.jpg?path=d29ya3NwYWNlJTJGSEJPVF9mbGFzayUyRmltYWdlcyUyRmNvdmlkLmpwZw==&docker_id=dp9Hbo59aZ4kbCsRvUr&secure_session_id=SISBOiU_mLy7fEo_rJB3dmz-W-TLxNvo"
           },
           "items": json_data["items"][:5]}}]}}
     
@@ -53,6 +53,30 @@ def push_statusImg() :
 }
 
 	return jsonify(imgSend)
+
+@app.route('/testing', methods=['POST'])
+def testing_Message(): 
+    content = request.get_json()
+    content = content['userRequest']
+    content = content['utterance']
+       
+    with open("testing_list.json") as json_file:
+        json_data = json.load(json_file)
+        
+    dataSend = {
+  "version": "2.0",
+  "template": {
+    "outputs": [
+      {
+        "listCard": {
+          "header": {
+            "title": "제주 공무원 시험 일정입니다!",
+            "imageUrl" : "https://proxy.goorm.io//service/5edf4a94cfcc8f3ea83d67ff_dp9Hbo59aZ4kbCsRvUr.run.goorm.io/9080//file/load/korea.png?path=d29ya3NwYWNlJTJGSEJPVF9mbGFzayUyRmltYWdlcyUyRmtvcmVhLnBuZw==&docker_id=dp9Hbo59aZ4kbCsRvUr&secure_session_id=SISBOiU_mLy7fEo_rJB3dmz-W-TLxNvo"
+            },
+          "items": json_data["items"]}}]}}
+    
+    return jsonify(dataSend)
+    
 
 if __name__ == "__main__" :
     app.run(host='0.0.0.0', port=5000)
